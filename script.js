da27480b7c9c391b329e5a6b48743db6ae28da49
Pikachu <jouxiong15@gmail.com>
2021-02-24 22:42:08 -0600

    added one question

---
+++ script.js
@@ -0,0 +1,12 @@
+// when start button is clicked, then the quiz begins
+
+// Initialized empty array to store the choices for each question
+var choices = [];
+
+// quiz questions
+var quiz =[
+    ['How would I make all the characters in a string capitalized?', 
+        choices[".toLowerCase()", ".toUpperCase", ".toUpperCase()", ".toCapitalized();"],
+        choices[2]]
+    
+];
