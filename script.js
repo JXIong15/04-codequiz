b44287725bf6627add697539e5febb1e408c3540
Pikachu <jouxiong15@gmail.com>
2021-02-27 15:02:03 -0600

    figured out how to get into arrays

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 var pageEl = document.getElementById("page");
-var timerEl = document.querySelector("time");
+var timerEl = document.querySelector("#time");
 
 // when start button is clicked, then the quiz begins
 var startButton = document.getElementById("start").addEventListener("click", generateQuiz);
 
 // MIGHT WANT TO MAKE THIS QUIZ ITS OWN JS FILE
-// Initialized empty array to store the choices for each question
-var choices = [];
 // quiz questions
 var quiz = [
-    ['How would I make all the characters in a string capitalized?',
-        choices[".toLowerCase()", ".toUpperCase", ".toUpperCase()", ".toCapitalized();"],
-        choices[2]],
-    ['Which is NOT a coding language?',
-        choices['Java', 'JavaScript', 'html', 'Python'],
-        choices[3]],
-    ['What would you use to iterate through an array of a set length?',
-        choices['while loop', 'iteration loop', 'loop', 'for loop'],
-        choices[3]],
-    ['How would you move up through the DOM tree?',
-        choices['.parent()', 'eq()', '.css()', '.branchUp'],
-        choices[0]],
-    ['How would you use git to upload your work to GitHub?',
-        choices['git upload', 'git push', 'git transfer', 'git pass'],
-        choices[1]]
+    {question: 'How would I make all the characters in a string capitalized?',
+     choices: [".toLowerCase()", ".toUpperCase", ".toUpperCase()", ".toCapitalized();"],
+     answer: ".toUpperCase()"},
+
+    {question: 'Which is NOT a coding language?',
+     choices: ['Java', 'JavaScript', 'html', 'Python'],
+     answer: 'html'},
+
+    {question: 'What would you use to iterate through an array of a set length?',
+     choices: ['while loop', 'iteration loop', 'loop', 'for loop'],
+     answer: 'loop'},
+
+    {question: 'How would you move up through the DOM tree?',
+     choices: ['.parent()', 'eq()', '.css()', '.branchUp'],
+     answer: '.parent()'},
+
+    {question: 'How would you use git to upload your work to GitHub?',
+     choices: ['git upload', 'git push', 'git transfer', 'git pass'],
+     answer: 'git push'}
 ];
-console.log(quiz[1]);
 
 function countdown() {
     var timeLeft = 30;
@@ -50,11 +51,11 @@ function generateQuiz() {
     var score = 0; // NEED TO DISPLAY THE SCORE
 
     // creates the choices
-    var choiceA = document.createElement("button");
+    var choiceA = $('<button>');
     var choiceB = document.createElement("button");
     var choiceC = document.createElement("button");
     var choiceD = document.createElement("button");
-
+    
     // interates through the array of the questions in the quiz.
     for (var i = 0; i < quiz.length; i++) {
         // pageEl.innerHTML= `<div> 
@@ -62,10 +63,10 @@ function generateQuiz() {
         // <button id='answer1' >answer1</button>
         // <button id='answer1' >answer1</button>
         // `;
-
+        
         var quizQuestion = {
-            question: quiz[i[0]],
-            // choiceA = quiz[i[1[0]]],
+            displayQuestion: quiz[i].question,
+            // choiceA = quiz[i].choices[0],
             // choiceB = quiz[i[1[1]]],
             // choiceC = quiz[i[1[2]]],
             // choiceD = quiz[i[1[3]]],
@@ -74,25 +75,28 @@ function generateQuiz() {
 
         pageEl.innerHTML = quizQuestion;
 
-        var userChoice = pick.addEventListener("click", generateQuiz);
-        if (userChoice == quiz[2]) {
-            score++;
-        }
-        else {
-            score--;
-        }
+        console.log(quizQuestion);
+
+        // var userChoice = pick.addEventListener("click", generateQuiz);
+        // if (userChoice == quiz[2]) {
+        //     score++;
+        // }
+        // else {
+        //     score--;
+        // }
     }
     
     displayResults();
 }
 
 function displayResults() {
-    if (timerEl > 0) {
-        page.textContent = "YOU WIN!!!🏆";
-        // allow user to submit their name and time
-    }
-    else {
-        page.textContext = "GAME OVER";
-    }
-    var resetButton = document.createElement("button").addEventListener("click", resetGame);
+    
+    // if (timerEl > 0) {
+    //     page.textContent = "YOU WIN!!!🏆";
+    //     // allow user to submit their name and time
+    // }
+    // else {
+    //     page.textContext = "GAME OVER";
+    // }
+    // var resetButton = document.createElement("button").addEventListener("click", resetGame);
 }
